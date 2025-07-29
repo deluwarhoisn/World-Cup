70489c1da5c0b4987cf55f15876df381fba2e8ed
deluwarhoisn <babuhossen301@gmail.com>
2025-07-29 20:24:30 +0600

    added loop

---
+++ javascript-2/js-loop/recap.js
@@ -0,0 +1,18 @@
+// const : value will not change মান পরিবর্তন হবে না
+
+// let: value can change মান পরিবর্তন হতে পারে
+// var: we should not use it আমাদের এটা ব্যবহার করা উচিত নয়।
+
+// variable চল
+const amount = 200;
+
+// condition অবস্থা
+if (amount > 100){
+
+}
+else {
+
+}
+
+// array অ্যারে
+const numbers = [12, 32, 54, 65];
