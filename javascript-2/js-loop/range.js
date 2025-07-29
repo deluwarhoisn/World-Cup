40fe0ed9b2a53dbd96cf520e0cbd47bb5e6ef816
deluwarhoisn <babuhossen301@gmail.com>
2025-07-29 22:18:48 +0600

    added whil and - +

---
+++ javascript-2/js-loop/range.js
@@ -0,0 +1,10 @@
+// let num = 1;
+// while (num < 10){  // focus on the condition অবস্থার উপর মনোযোগ দিন
+//     console.log(num);
+//     num++;
+// }
+let num = 50;
+while (num <= 100){
+    console.log(num);
+    num ++;
+}
