0564e6697776a7f7f37f24e364143e14565cecc3
deluwarhoisn <babuhossen301@gmail.com>
2025-07-29 23:02:12 +0600

    added i--- and i++

---
+++ javascript-2/js-loop/decrimantal-for.js
@@ -0,0 +1,9 @@
+// incremental loop ক্রমবর্ধমান লুপ
+
+for (let i = 0; i <=10; i++){
+    console.log(i);
+}
+// decremental for loop লুপের জন্য হ্রাসকারী
+for (let i =10; i <=10; i--){
+    console.log(i);
+}
