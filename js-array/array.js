8d2b4a03579490db8013a3b5f26634e62bba2802
deluwarhoisn <babuhossen301@gmail.com>
2025-07-30 20:25:12 +0600

    added array

---
+++ js-array/array.js
@@ -0,0 +1,4 @@
+const number = [12, 22, 54, 65];
+const friends = ['abul', 'babul', 'cabul', 'dadul', 'bulbul'];
+const passed = [true, false, true, false, true];
+const mixedArray = [12, 'Alahbad', terue ,45.566];
