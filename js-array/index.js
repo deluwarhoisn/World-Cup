e06c669b9cc8199c15c7acc53309aff9bff8573c
deluwarhoisn <babuhossen301@gmail.com>
2025-07-30 20:56:41 +0600

    added index.js

---
+++ js-array/index.js
@@ -0,0 +1,8 @@
+const number =[1, 30,40, 21, 4, 5];
+console.log(number);
+console.log(number[3]);
+//  get element value by index
+const one = number[3];
+console.log(number[3]);
+
+// set or 
---
+++ js-array/lenght.js
@@ -0,0 +1,2 @@
+const number = [1, 5, 66, 11, 45, 65,];
+console.log(number.length);
