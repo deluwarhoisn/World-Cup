5ff1f6333efa1a88ec86e2d14e5177ba5e503d9c
deluwarhoisn <babuhossen301@gmail.com>
2025-07-31 13:41:49 +0600

    added comment out

--- js-array/index.js
+++ js-array/index.js
@@ -1,9 +1,9 @@
-// const number =[1, 30,40, 21, 4, 5];
-// console.log(number);
-// console.log(number[3]);
-// //  get element value by index
-// const one = number[3];
-// console.log(number[3]);
+const number =[1, 30,40, 21, 4, 5];
+console.log(number);
+console.log(number[3]);
+//  get element value by index
+const one = number[3];
+console.log(number[3]);
 
 // // set or 
 const number = [0,112,232,453,464,765,76,907,888,79];
