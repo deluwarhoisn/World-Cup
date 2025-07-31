de578b69141fc79efe55e30ad60dab6134800a8a
deluwarhoisn <babuhossen301@gmail.com>
2025-07-31 13:40:04 +0600

    aded index practish

--- js-array/index.js
+++ js-array/index.js
@@ -1,8 +1,13 @@
-const number =[1, 30,40, 21, 4, 5];
-console.log(number);
-console.log(number[3]);
-//  get element value by index
-const one = number[3];
-console.log(number[3]);
+// const number =[1, 30,40, 21, 4, 5];
+// console.log(number);
+// console.log(number[3]);
+// //  get element value by index
+// const one = number[3];
+// console.log(number[3]);
 
-// set or 
+// // set or 
+const number = [0,112,232,453,464,765,76,907,888,79];
+console.log(number);
+console.log(number[2]);
+const fourth = number[3]
+console.log(fourth);
--- js-array/lenght.js
+++ js-array/lenght.js
@@ -1,2 +1,2 @@
-const number = [1, 5, 66, 11, 45, 65,];
+const number = [1, 5, 66, 11, 45, 65,22,590,  66, 11, 45, 65,22,590,767,];
 console.log(number.length);
